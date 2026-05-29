88be4a8a6f824132982dd57190d713418e638e7f
ansh012006 <ansh012006@gmail.com>
2026-05-30 02:36:13 +0530

    Add files via upload

---
+++ inventory/static/firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyD7AojYDPZEk6HjDNLLIXlv5qoA0dZbqF4",
+  authDomain: "srmc-5d645.firebaseapp.com",
+  projectId: "srmc-5d645",
+  messagingSenderId: "694287683451",
+  appId: "1:694287683451:web:29ba8eb1eb4e249afff7de",
+});
+
+const messaging = firebase.messaging();
